f4770dd6dac1f7466be5e13f78935b7bb117213e
Daniel-Ekerhovd <danielekerh@gmail.com>
2023-10-13 15:46:53 +0200

    FINITO BURRITO QUESEDIA

--- js/product.js
+++ js/product.js
@@ -3,3 +3,24 @@ const product = {
     price: 99,
     onSale: true,
 };
+
+let productContainer = document.querySelector(".container");
+let nameUnderline = "none"
+let productPrice = "Unavailable";
+let productSale = "NOT";
+
+if (product.price) {
+    productPrice = product.price + " $"
+
+}
+
+if (product.onSale) {
+    productSale = ""
+    nameUnderline = "underline"
+
+}
+
+productContainer.innerHTML += 
+`<h1 style="text-decoration: ${nameUnderline}">${product.name}</h1>
+    <p>Pricing: ${productPrice}</p>
+    <p>This product is ${productSale} on sale!</p>`;
--- js/products.js
+++ js/products.js
@@ -14,3 +14,40 @@ const products = [
         soldOut: true,
     },
 ];
+
+
+let productCard = document.querySelector(".container");
+let html = "";
+
+
+
+
+for (let i = 0; i < products.length; i++) {
+
+    let name = products[i].name;
+    let pricing = products[i].price + " $";
+    let stock = products[i].soldOut;
+    let stockColor = "#008000";
+
+    stock = "In stock";
+
+
+
+    if (!products[i].soldOut) {
+        stock = "Sold Out"
+        stockColor = "red";
+    }
+
+    if (!products[i].price) {
+        pricing = "Pricing Unavailable"
+        
+    }; 
+
+
+    html += `<h1>${name}</h1>
+                <p>Cost: ${pricing}</p>
+                <p>Stock: <span style="color: ${stockColor}">${stock}</span></p>
+`    
+}
+
+productCard.innerHTML = html;
